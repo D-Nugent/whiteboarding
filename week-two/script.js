5f89c9f1de2be0fa911a969c45a188ba2cf77af1
David Nugent <dnugent@brainstation.io>
2021-03-16 11:32:12 -0700

    Added some new questions and answers, tidied up code

--- week-two/script.js
+++ week-two/script.js
@@ -1,19 +1,71 @@
 console.log("---Let's Learn JavaScript---")
 
 // ---Variables & Scope---
+console.log('// ---Variables & Scope---');
 
 // 1. How do you assign a variable?
+console.log('// 1. How do you assign a variable?');
 
 // Ans
 let mightChange = [1, 2, 3];
 const shouldNotChange = {name: 'Mark', age: 26};
-var imNotBestPractice = `There is ${mightChange[0]} person named ${shouldNotChange.name}`;
+var imGloballyScoped = `There is ${mightChange[0]} person named ${shouldNotChange.name}`;
 
 console.log(mightChange);
 console.log(shouldNotChange);
-console.log(imNotBestPractice);
+console.log(imGloballyScoped);
 
+// 1b. What are the different data types in JavaScript?
+console.log('// 1b. What are the different data types in JavaScript?');
+
+//Ans
+let string = 'Hi there!';
+let number = 26;
+let boolean = true;
+// let undefined;
+let nullType = null; //Not equivalent to '' or 0;
+let object = {
+  name: 'Darth Vaders',
+  howCool: 'Very Cool',
+}
+let array = ['Darth Vaders', 'Very cool', 6]
+let functionType = function(){
+  return 'This is a function'
+} //Technically also just an object but noteworthy
+
+console.log(string, number, boolean, nullType, object, array, functionType);
+
+// 1c. Try creating an object which contains a string, number, boolean and array
+console.log('// 1c. Try creating an object which contains a string, number, boolean and array');
+
+//Ans
+let darthVaders = {
+  howCool: 'Very Cool',
+  numberOfPeople: 6,
+  goingToDoGreatInCourse: true,
+  people: ['David', 'Peter', 'Charles', 'Tim', 'Noble', 'Jensen'],
+}
+console.log(darthVaders);
+// 1d. Try creating an array which contains two objects
+console.log('// 1d. Try creating an array which contains two objects');
+
+//Ans
+let darthVaderPeople =
+[
+  {
+    name: 'David',
+    position: 'TA',
+    tired: true,
+  },
+  {
+    name: 'Jensen',
+    position: 'Cool Student',
+    tired: false
+  }
+]
+console.log(darthVaderPeople);
 // 2. What is the difference between let and var?
+console.log('// 2. What is the difference between let and var?');
 
 // Ans
 // TLDR: Scope and redeclaration
@@ -32,18 +84,22 @@ function adjustScope() {
 
     iWannaBeGlobal();
 
+    //2a
     // console.log('This will fail due to Scope:');
     // console.log(`${a}, ${b}, ${c}, ${d}`);
     
+    //2c
     // let c = 'Goodbye';
     // let d = 'Global';
     // console.log(`${a}, ${b}, ${c}, ${d}`);
 
+    //2d
     // // 'This will fail as you cant redeclare with let:
     // let c = 'Greatbye';
     // let d = 'International';
     // console.log(`${a}, ${b}, ${c}, ${d}`);
     
+    //2e
     // If the variables were declared with var you can redeclare with var:
     // var c = 'Greatbye';
     // var d = 'International';
@@ -53,41 +109,55 @@ function adjustScope() {
 adjustScope();
 
 // ---Control Flow (Conditionals)---
+console.log('// ---Control Flow (Conditionals)---');
 
 // 1. What are some examples of control flow
+console.log('// 1. What are some examples of control flow');
 
 // Ans: if (elseif, else), switch, while
 
-// 2. Write a statement which includes an if, elseif and else statement which console.log's if a pet is a cat, bird or t-rex
-let petCat = {mainAttribute: 'furry'}
-let petBird = {mainAttribute: 'wings'}
-let petTrex = {mainAttribute: '12 feet tall'}
-let petTrexFull = {heightInFeet:12, teeth:'sharp', tail:true,wings:false}
+// 2. Write a statement using the below object which includes an if, elseif and else statement which console.log's if a pet is a cat, bird or t-rex
+console.log(`// 2. Write a statement using the below object which includes an if, elseif and else statement which console.log's if a pet is a cat, bird or t-rex`);
 
-let pet = {mainAttribute: 'furry'}
+let pet = 'furry'
 
-if (pet.mainAttribute === 'furry') {
+if (pet === 'furry') {
     console.log(`It's a cat`);
-} else if (pet.mainAttribute === 'wings') {
+} else if (pet === 'wings') {
     console.log(`It's a bird`);
 } else {
     console.log(`It's a T-rex`);
 }
-// 3. Change this to a switch statement
 
-switch (pet.mainAttribute) {
-    case 'furry':
-        console.log(`It's a cat`);
-        break;
-    case 'wings':
-        console.log(`It's a bird`);
-        break;
-    default:
-        console.log(`It's a T-Rex`);
-        break;
+//2b. Try experimenting with how you might redo the above if you have the following object:
+console.log('//2b. Try experimenting with how you might redo the above if you have the following object:');
+let petTwo = {heightInFeet:12, teeth:'sharp', tail:true,wings:false}
+
+if (petTwo.heightInFeet > 7) {
+  console.log(`It's a T-Rex`);
+} else if (petTwo.wings === true) {
+  console.log(`It's a bird`);
+} else {
+  console.log(`It's a T-rex`);
 }
 
-// 4. What is a While Loop?
+// 3. Change this to a switch statement
+console.log('// 3. Change this to a switch statement');
+
+// switch (pet.mainAttribute) {
+//     case 'furry':
+//         console.log(`It's a cat`);
+//         break;
+//     case 'wings':
+//         console.log(`It's a bird`);
+//         break;
+//     default:
+//         console.log(`It's a T-Rex`);
+//         break;
+// }
+
+// 4. What is a While Loop? Try writing a loop.
+console.log('// 4. What is a While Loop? Try writing a loop.');
 
 // Ans: a statement which runs as long as a condition evaluates to true 
 let i = 0;
@@ -96,9 +166,10 @@ while (i<4) {
     i++;
 }
 
-// ---Control Flow (Loops)---
+console.log('// ---Control Flow (Loops)---');
 
 // 1. What values do you expect to come out of the following statement:
+console.log('// 1. What values do you expect to come out of the following statement:');
 let birthYear = [2006, 1967, 2012, 1985, 1975]
 
 for(let i = 0; i < birthYear.length; i++) {
@@ -119,6 +190,7 @@ for(let i = 0; i < birthYear.length; i++) {
 // 5) 45 - You're an adult
 
 // 2. How would you write this as a forEach loop?
+console.log('// 2. How would you write this as a forEach loop?');
 
 birthYear.forEach(year => {
     let age = 2021 - year;
@@ -131,6 +203,7 @@ birthYear.forEach(year => {
 });
 
 // 3. Combining Conditional and Loop Statements: 
+console.log('// 3. Combining Conditional and Loop Statements:'); 
 // Write a for loop that goes through the numbers 1-12 and depending on the following, prints something to the console. 
 // For numbers 1 through 6, print '...loading' to the console
 // For numbers 7 through 10, print '...almost there!' to the console
@@ -152,24 +225,3 @@ for (let i = 1; i < 13; i++) {
       console.log('DONE')
     }
   }
-  
-
-
-  // If there is a student who is finding content too easy and needs a challenge:
-
-  // Bonus: Create a function that console logs a pyramid:
-function triangle(size){
-	for(let i = 1; i <= size; i++){ 
-		let array = [];
-		for(let j = size - i; j >=  0; j--){
-			array.push(` `);
-        }
-
-        for(let k = 0; k < (i * 2 - 1); k++){
-		array.push(`#`);
-        }
-		console.log(array.join(``));
-	}
-}
-
-triangle(5);
